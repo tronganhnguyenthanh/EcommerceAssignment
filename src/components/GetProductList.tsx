38db7aaff429af472ed3d05ae7c87f80ceb51a12
Phuong Nguyen <tronganhnguyen222@gmail.com>
2023-09-15 08:59:40 +0700

    [UPDATE][FE] add loading items

--- src/components/GetProductList.tsx
+++ src/components/GetProductList.tsx
@@ -1,9 +1,12 @@
-import { useEffect, useState } from "react"
+import {useEffect, useState} from "react"
 import productListTypes from "../types/product.list.types"
 import "../css/product.css"
+import InfiniteScroll from "react-infinite-scroll-component"
+import loadingApi from "../images/loading.gif"
 const GetProductList = () => {
   const [productList, setProductList] = useState<productListTypes | undefined>()
   const [keyword, setKeyword] = useState("")
+  const [hasMore, setHasMore] = useState(false)
   const handleOnFilterProduct = async (e: any) => {
     e.target.value = e.target.value.trim();
     setKeyword(e.target.value)
@@ -13,13 +16,18 @@ const GetProductList = () => {
     setProductList(searchableProduct)
   }
   useEffect(() => {
-    getProductList()
+   getProductList()
   }, [])
   const getProductList = async () => {
     let productApi = "https://dummyjson.com/products"
     let productList = await fetch(productApi)
     let response = await productList.json()
     setProductList(response)
+    setHasMore(!hasMore)
+  }
+  const styles = {
+    display:"flex",
+    justifyContent:"center"
   }
   return (
     <div className="container">
@@ -28,26 +36,37 @@ const GetProductList = () => {
         <input
           type="text"
           className="mt-2 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-9/12 ml-4 rounded-md sm:text-sm focus:ring-1"
-          style={{ margin: "auto", borderRadius: "30px" }}
+          style={{margin:"auto", borderRadius:"30px"}}
           onChange={handleOnFilterProduct}
         />
       </form>
-      <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-10 p-2">
-        {productList?.products?.map((i, index) => {
-          return (
-            <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl" key={index}>
-              <div className="md:flex p-2">
-                <img src={String(i?.thumbnail)} alt="" className="image-product-custom" />
-              </div>
-              <div className="p-8">
-                <h1 className="text-center text-2xl text-blue-500">{i?.title}</h1>
-                <p className="text-center text-orange-300">{"$" + Number(i?.price)}</p>
-              </div>
-            </div>
-          )
-        })
+      <InfiniteScroll 
+         dataLength={Number(productList?.products?.length)}
+         next={getProductList}
+         hasMore={!hasMore}
+         loader={
+          <div style={styles} className="py-48">
+            <img src={loadingApi} alt="" className="w-12"/>
+          </div>
         }
-      </div>
+       >
+        <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-10 p-2">
+          {productList?.products?.map((i, index) => {
+            return (
+              <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl" key={index}>
+                <div className="md:flex p-2">
+                  <img src={String(i?.thumbnail)} alt="" className="image-product-custom" />
+                </div>
+                <div className="p-8">
+                  <h1 className="text-center text-2xl text-blue-500">{i?.title}</h1>
+                  <p className="text-center text-orange-300">{"$" + Number(i?.price)}</p>
+                </div>
+              </div>
+            )
+          })
+          }
+        </div>
+      </InfiniteScroll>
     </div>
   )
 }
