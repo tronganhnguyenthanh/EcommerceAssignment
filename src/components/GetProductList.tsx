041d48f75da28f531bdb8adbb4569338ef7fb552
Phuong Nguyen <tronganhnguyen222@gmail.com>
2023-09-17 11:14:10 +0700

    [UPDATE][FE] fix error cannot post in form

--- src/components/GetProductList.tsx
+++ src/components/GetProductList.tsx
@@ -7,14 +7,15 @@ const GetProductList = () => {
   const [productList, setProductList] = useState<productListTypes | undefined>()
   const [keyword, setKeyword] = useState("")
   const [hasMore, setHasMore] = useState(false)
-  const handleOnFilterProduct = async (e: any) => {
+  // const [error, setError] = useState(false)
+  const handleOnFilterProduct = async (e:any) => {
     e.target.value = e.target.value.trim();
     setKeyword(e.target.value)
     let filterProductApi = `https://dummyjson.com/products/search?q=${keyword}`
     let filterProduct = await fetch(filterProductApi)
     let searchableProduct = await filterProduct?.json()
     setProductList(searchableProduct)
-  }
+   }
   useEffect(() => {
    getProductList()
   }, [])
@@ -28,22 +29,20 @@ const GetProductList = () => {
   return (
     <div className="container">
       <h1 className="text-3xl text-center text-blue-500">Get product list</h1>
-      <form method="POST">
-        <input
-          type="text"
-          className="px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-9/12 ml-4 rounded-md sm:text-sm focus:ring-1"
-          style={{margin:"auto", borderRadius:"30px"}}
-          placeholder="Search by keyword"
-          onChange={handleOnFilterProduct}
-        />
-      </form>
+       <input
+         type="text"
+         className="px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-9/12 ml-4 rounded-md sm:text-sm focus:ring-1"
+         style={{margin:"auto", borderRadius:"30px"}}
+         placeholder="Search by keyword"
+         onChange={handleOnFilterProduct}
+       />
       <InfiniteScroll 
          dataLength={Number(productList?.products?.length)}
          next={getProductList}
          hasMore={!hasMore}
          loader={<Loader/>}
        >
-        <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-10 p-2">
+       <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-10 p-2">
           {productList?.products?.map((i, index) => {
             return (
               <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl" key={index}>
@@ -58,7 +57,7 @@ const GetProductList = () => {
             )
           })
           }
-        </div>
+       </div>
       </InfiniteScroll>
     </div>
   )
